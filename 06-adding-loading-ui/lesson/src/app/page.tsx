9cfb317c13459ebe814079870bba4c4c74773201
Francisco <javiermartincuenca@gmail.com>
2023-10-17 22:17:54 +0200

    Lesson 6 completed

---
+++ 06-adding-loading-ui/lesson/src/app/page.tsx
@@ -0,0 +1,197 @@
+import { prisma } from "@/lib/prisma";
+import { ChevronRightIcon } from "@heroicons/react/20/solid";
+import Link from "next/link";
+import { SearchInput } from "./SearchInput";
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | string[] | undefined };
+}) {
+  // To simulate delay
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+
+  const search =
+    typeof searchParams.search === "string" ? searchParams.search : undefined;
+  const perPage = 6;
+  const totalUsers = await prisma.user.count({
+    where: {
+      name: {
+        contains: search,
+      },
+    },
+  });
+  const totalPages = Math.ceil(totalUsers / perPage);
+  const page: number =
+    typeof searchParams.page === "string" &&
+    +searchParams.page > 1 &&
+    +searchParams.page <= totalPages
+      ? +searchParams.page
+      : 1;
+  const users = await prisma.user.findMany({
+    take: perPage,
+    skip: (page - 1) * perPage,
+    where: {
+      name: {
+        contains: search,
+      },
+    },
+  });
+  const currentSearchParams = new URLSearchParams();
+
+  if (search) {
+    currentSearchParams.set("search", search);
+  }
+
+  if (page > 1) {
+    currentSearchParams.set("page", `${page}`);
+  }
+
+  return (
+    <div className="px-8 bg-gray-50 pt-12 min-h-screen">
+      <div className="flex items-center justify-between">
+        <div className="w-80">
+          <SearchInput search={search} />
+        </div>
+        <div className="mt-0 ml-16 flex-none">
+          <button
+            type="button"
+            className="block rounded-md bg-indigo-600 py-1.5 px-3 text-center text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            Add user
+          </button>
+        </div>
+      </div>
+      <div className="mt-8 flow-root">
+        <div className="-my-2 -mx-6">
+          <div className="inline-block min-w-full py-2 align-middle px-6">
+            <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 rounded-lg">
+              <table className="min-w-full divide-y divide-gray-300">
+                <thead className="bg-gray-50">
+                  <tr>
+                    <th className="py-3.5 pr-3 text-left text-sm font-semibold w-[62px] sm:w-auto text-gray-900 pl-4">
+                      ID
+                    </th>
+                    <th className="px-3 py-3.5 text-left text-sm font-semibold w-[130px] sm:w-auto text-gray-900">
+                      Name
+                    </th>
+                    <th className="px-3 py-3.5 text-left text-sm font-semibold w-[175px] sm:w-auto text-gray-900">
+                      Email
+                    </th>
+                    <th className="relative py-3.5 pl-3 pr-4">
+                      <span className="sr-only">Edit</span>
+                    </th>
+                  </tr>
+                </thead>
+                <tbody className="divide-y divide-gray-200 bg-white">
+                  {users.map((user) => (
+                    <tr key={user.id}>
+                      <td className="whitespace-nowrap py-4 pr-3 text-sm font-medium text-gray-900 pl-4">
+                        {user.id}
+                      </td>
+                      <td className="whitespace-nowrap px-3 py-4 text-sm font-medium max-w-[130px] sm:w-auto truncate">
+                        {user.name}
+                      </td>
+                      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500 max-w-[175px] sm:w-auto truncate">
+                        {user.email}
+                      </td>
+                      <td className="relative whitespace-nowrap py-4 pl-4 pr-4 text-right text-sm font-medium">
+                        <a
+                          href="#"
+                          className="text-indigo-600 hover:text-indigo-900 inline-flex items-center"
+                        >
+                          Edit
+                          <ChevronRightIcon className="w-4 h-4" />
+                        </a>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="mt-4 flex items-center justify-between">
+        <p className="text-sm text-gray-700">
+          Showing{" "}
+          <span className="font-semibold">{(page - 1) * perPage + 1}</span> to{" "}
+          <span className="font-semibold">
+            {Math.min(page * perPage, totalUsers)}
+          </span>{" "}
+          of <span className="font-semibold">{totalUsers}</span> users
+        </p>
+        <div className="space-x-2">
+          <PreviousPage page={page} currentSearchParams={currentSearchParams} />
+          <NextPage
+            page={page}
+            totalPages={totalPages}
+            currentSearchParams={currentSearchParams}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function PreviousPage({
+  page,
+  currentSearchParams,
+}: {
+  page: number;
+  currentSearchParams: URLSearchParams;
+}) {
+  const newSearchParams = new URLSearchParams(currentSearchParams);
+
+  if (page > 2) {
+    newSearchParams.set("page", `${page - 1}`);
+  } else {
+    newSearchParams.delete("page");
+  }
+
+  return page > 1 ? (
+    <Link
+      href={`/?${newSearchParams}`}
+      className="inline-flex items-center justify-center rounded-md border border-gray-300 bg-white px-3 py-2 text-sm font-semibold text-gray-900 hover:bg-gray-50"
+    >
+      Previous
+    </Link>
+  ) : (
+    <button
+      disabled
+      className="inline-flex items-center justify-center rounded-md border border-gray-300 bg-white px-3 py-2 text-sm font-semibold text-gray-900 opacity-50"
+    >
+      Previous
+    </button>
+  );
+}
+
+function NextPage({
+  page,
+  totalPages,
+  currentSearchParams,
+}: {
+  page: number;
+  totalPages: number;
+  currentSearchParams: URLSearchParams;
+}) {
+  const nextPageSearchParams = new URLSearchParams(currentSearchParams);
+
+  nextPageSearchParams.set("page", `${page + 1}`);
+
+  return page < totalPages ? (
+    <Link
+      href={`/?${nextPageSearchParams}`}
+      className="inline-flex items-center justify-center rounded-md border border-gray-300 bg-white px-3 py-2 text-sm font-semibold text-gray-900 hover:bg-gray-50"
+    >
+      Next
+    </Link>
+  ) : (
+    <button
+      disabled
+      className="inline-flex items-center justify-center rounded-md border border-gray-300 bg-white px-3 py-2 text-sm font-semibold text-gray-900 opacity-50"
+    >
+      Next
+    </button>
+  );
+}
